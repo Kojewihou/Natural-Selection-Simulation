a586f0f17f2373e54ed46bfaba36ce2f9546d477
kojewihou <103379288+Kojewihou@users.noreply.github.com>
2023-08-29 22:06:07 +0100

    v1.0.1
    Included save as GIF functionality

--- main.py
+++ main.py
@@ -17,26 +17,27 @@ def RunRound(organismArray, foodArray, frameRate):
     desired_interval = 2.5  # Desired interval in second
     directionChangeProbability = 1 - \
         np.exp(-1 / (frameRate * desired_interval))
-    
+
     roundStartTime = time.time()
 
     for roundNumber in range(1, roundLength):
         start_time = time.time()
         organismArray = NextMove(
             organismArray, roundNumber, boundaryRadius, directionChangeProbability)
-        #elapsed_time = time.time() - start_time
-        #print(f"Frame-{roundNumber + 1} movement took {elapsed_time:.6f} seconds to simulate.")
+        # elapsed_time = time.time() - start_time
+        # print(f"Frame-{roundNumber + 1} movement took {elapsed_time:.6f} seconds to simulate.")
         organismArray, foodArray = FoodCollision(
             organismArray, foodArray, roundNumber)
         elapsed_time = time.time() - start_time
-        #print( f"Frame-{roundNumber} collision took {elapsed_time:.6f} seconds to simulate.")
+        # print( f"Frame-{roundNumber} collision took {elapsed_time:.6f} seconds to simulate.")
         # time.sleep(1)
 
     elapsedRoundTime = time.time() - roundStartTime
 
     print(f"Round  took {elapsedRoundTime:.6f} seconds to simulate.")
 
-    percentConsumed = (np.sum(organismArray[5, :, -1]) / foodArrayDisplay.shape[1]) * 100
+    percentConsumed = (
+        np.sum(organismArray[5, :, -1]) / foodArrayDisplay.shape[1]) * 100
     print(f"Percentage of Food Consumed: {percentConsumed}%")
     return organismArray, foodArrayDisplay
 
@@ -45,33 +46,36 @@ def visualize(tuple, boundaryRadius, frameRate):
     simulationMatrix, foodArray = tuple
     time_steps = simulationMatrix.shape[2]
 
-
     fig, ax = plt.subplots()
 
-    wood =Circle((0,0), boundaryRadius, fill=True, color='green', linewidth=2, alpha=0.5)
-    home =Circle((0,0), boundaryRadius*0.2, fill=True, color='red', linewidth=2, alpha=0.2)
+    wood = Circle((0, 0), boundaryRadius, fill=True,
+                  color='green', linewidth=2, alpha=0.5)
+    home = Circle((0, 0), boundaryRadius*0.2, fill=True,
+                  color='red', linewidth=2, alpha=0.2)
     square_center = (0, 0)  # Square center coordinates (x, y)
     square_side_length = 0.15*boundaryRadius    # Length of each side of the square
     house = Rectangle((square_center[0] - square_side_length/2, square_center[1] - square_side_length/2),
-                   square_side_length, square_side_length, fill=True, color='black', linewidth=2)
-
+                      square_side_length, square_side_length, fill=True, color='black', linewidth=2)
 
     ax.add_patch(wood)
     ax.add_patch(home)
     sc = ax.scatter([], [], c='r', marker='D')
     ax.set_title('Animated Scatter Plot')
     ax.set_xlabel('X Position')
-    ax.set_ylabel('Y Position') 
-    ax.set_xlim(-boundaryRadius, boundaryRadius)  # Adjust limits based on your data
-    ax.set_ylim(-boundaryRadius, boundaryRadius)  # Adjust limits based on your data
+    ax.set_ylabel('Y Position')
+    # Adjust limits based on your data
+    ax.set_xlim(-boundaryRadius, boundaryRadius)
+    # Adjust limits based on your data
+    ax.set_ylim(-boundaryRadius, boundaryRadius)
     ax.axis("off")
-    
+
     ax.add_patch(house)
 
     ax2 = ax.twinx()
     ax2.scatter(foodArray[0, :], foodArray[1, :],
                 c='g', marker='8', label='Tree')
-    ax2.set_xlim(-boundaryRadius, boundaryRadius)  # Adjust limits based on your data
+    # Adjust limits based on your data
+    ax2.set_xlim(-boundaryRadius, boundaryRadius)
     ax2.set_ylim(-boundaryRadius, boundaryRadius)
     ax2.axis("off")  # Adjust limits based on your data
     ax2.legend()
@@ -84,20 +88,21 @@ def visualize(tuple, boundaryRadius, frameRate):
         ax.set_title(
             f'Food Consumed: {np.sum(current_positions[5,:])}  - F{frame}')
 
-
     animation = FuncAnimation(
         fig, animate, frames=time_steps, interval=1000/200)
     plt.show()
-    print ("run")
+    plt.close()
+    
     path = "C:\\Users\\Elliot\\Documents\\Git\\Natural-Selection-Simulation\\Animations\\round.gif"
-    animation.save(path, writer='pillow', fps=frameRate)
+    #animation.save(path, writer='pillow', fps=frameRate)
 
 
 if __name__ == "__main__":
-    frameRate = 60
+    frameRate = 24
     boundaryRadius = 200
-    roundLength = 30
-    organismArray = ConstructOrganismArray(600, boundaryRadius, roundLength,frameRate)
-    foodArray = ConstructFoodArray(70, boundaryRadius)
+    roundLength = 10
+    organismArray = ConstructOrganismArray(
+        6000, boundaryRadius, roundLength, frameRate)
+    foodArray = ConstructFoodArray(200, boundaryRadius)
     roundResult = RunRound(organismArray, foodArray, frameRate)
-    visualize(roundResult,boundaryRadius, frameRate)
+    visualize(roundResult, boundaryRadius, frameRate)
